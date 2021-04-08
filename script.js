65cabd2240b1e8fa99a5f6cb10546c5ae867a47d
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-04-08 17:11:01 +0500

    create count and restart play

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 'use strict';
+
 const isNumder = function(n) {
   if(n === null) {
     return false;
   }
   return (isNaN(n) || n.trim() === '');
-}
+};
+
 const one = function() {
-  const value = 26;
+  const value = Math.round(Math.random() * 99 + 1);
+  let count = 10;
   let answer = prompt('Угадай число от 1 до 100');
   
   function two() {
+    if(count === 1) {
+      return 'Попытки закончились, хотите сыграть еще?';
+    }
     if(isNumder(answer)) {
       answer = prompt('Введите число!');
       return two();
@@ -18,18 +24,25 @@ const one = function() {
     }
     answer = parseFloat(answer);
     if(value === answer) {
-      return 'Поздравляю! Вы угадали!!!';
+      return 'Поздравляю! Вы угадали!!! Хотели бы сыграть еще?';
     } else if(value > answer) {
-      answer = prompt('Загаданное число больше');
+      count--;
+      answer = prompt('Загаданное число больше, осталось попыток ' + count);
       return two();
     } else if (value < answer) {
-      answer = prompt('Загаданное число меньше');
+      count--;
+      answer = prompt('Загаданное число меньше, осталось попыток ' + count);
       return two();
     } else if (value === null) {
       return 'Игра  окончена';
     } 
   }
-  return two();
-}
 
-console.log(one());
+  return two();
+};
+
+let restar = confirm(one());
+
+if(restar === true) {
+  restar = confirm(one());
+}
